6b4316ac3f660ee6fa1c6a457563891646987ba1
Aram-Gholikimilan <Aram.Gholikimilan@city.ac.uk>
2024-03-02 17:33:47 +0000

    added my details

--- src/TemporaryNode.java
+++ src/TemporaryNode.java
@@ -2,9 +2,9 @@
 // Coursework 2023/2024
 //
 // Submission by
-// YOUR_NAME_GOES_HERE
-// YOUR_STUDENT_ID_NUMBER_GOES_HERE
-// YOUR_EMAIL_GOES_HERE
+// Aram Gholikimilan
+// 220036178
+// Aram.Gholikimilan@city.ac.uk
 
 
 // DO NOT EDIT starts
