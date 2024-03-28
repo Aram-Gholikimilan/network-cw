d993c9eef2c788b568c131d8f174bf7ff8801091
Aram-Gholikimilan <Aram.Gholikimilan@city.ac.uk>
2024-03-28 21:28:55 +0000

    testing

--- src/TemporaryNode.java
+++ src/TemporaryNode.java
@@ -279,6 +279,8 @@ public class TemporaryNode implements TemporaryNodeInterface {
                     String nodeName = lines[i - 1]; // Adjust index for node name
                     String nodeAddress = lines[i]; // Adjust index for node address
 
+                    System.out.println(nodeName);
+                    System.out.println(nodeAddress);
                     String value = attemptGetFromNode(nodeName, nodeAddress, key);
                     if (value != null && !value.equals("NOPE")) {
                         System.out.println("Successfully retrieved value from fallback node: " + nodeName);
